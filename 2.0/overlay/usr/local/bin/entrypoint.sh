d59a41c1f0e1758b21667688c1b87d05a859818d
Thorsten Schifferdecker <ts@systs.org>
2020-01-03 22:05:41 +0100

    (feat) add haproxy v2.0 tree for k8scp

---
+++ 2.0/overlay/usr/local/bin/entrypoint.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+set -e
+
+# set defaults
+KUBEAPI_VIP_PORT="${KUBEAPI_VIP_PORT:-6443}"
+KUBEAPI_VIP_STATS_PORT="${KUBEAPI_VIP_STATS_PORT:-9000}"
+HAPROXY_CONF="/usr/local/etc/haproxy/haproxy.cfg"
+HAPROXY_CONF_TPL="${HAPROXY_CONF}.tpl"
+
+# checks
+if [ "x$KUBEAPI_VIP" == "x" ]; then
+  echo "Error, no environment KUBEAPI_VIP set, abort"
+  exit 1;
+fi
+
+if [ "x$HAPROXY_BACKENDS" == "x" ]; then
+  echo "Error, no environment HAPROXY_BACKENDS set, abort"
+  exit 1;
+fi
+
+# create config from template
+sed \
+  -e "s#KUBEAPI_VIP_PORT#$KUBEAPI_VIP_PORT#g" \
+  -e "s#KUBEAPI_VIP_STATS_PORT#$KUBEAPI_VIP_STATS_PORT#g" \
+  -e "s#KUBEAPI_VIP#$KUBEAPI_VIP#g" \
+  $HAPROXY_CONF_TPL > $HAPROXY_CONF
+
+echo $HAPROXY_BACKENDS | sed 's#,#\n#g' | while IFS= read -r backend
+do
+  no=$(($no+1))
+  if [ "x$(echo $backend | cut -d ':' -f 2 -s)" == "x" ]; then
+    BACKEND_PORT=":6443"
+  fi
+  echo "  server cp-$no $backend$BACKEND_PORT check check-ssl verify none" \
+    >> $HAPROXY_CONF
+done
+
+exec haproxy -W -db -f $HAPROXY_CONF
